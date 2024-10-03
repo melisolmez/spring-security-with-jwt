fcd57cd36807d75b2431c136b5a1e7e9a7cd03af
Melis Olmez <olmezmelis204@gmail.com>
2024-10-03 21:56:44 +0300

    update app config

--- src/main/java/com/melis/security/config/ApplicationConfig.java
+++ src/main/java/com/melis/security/config/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,8 +38,8 @@ public class ApplicationConfig {
         return config.getAuthenticationManager();
     }
 
-
-    private PasswordEncoder passwordEncoder() {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
